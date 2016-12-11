fad3064f69e87b705154ccc351dc72ae7fc990ce
fogside <cheskidova@phystech.edu>
2016-12-11 15:14:06 +0300

    gencode_data_update

--- scripts/my_model/launch_coding.sh
+++ scripts/my_model/launch_coding.sh
@@ -1,1 +1,1 @@
-python feature_extract.py -g ~/lncRNA/data/known_genes_19.fasta --gtf ~/lncRNA/data/hg19_description.gtf -l 1 --fformat gtf -o ./coding_table.csv -x Human_Hexamer.tsv
+python feature_extract.py -g ~/lncRNA/data/gencode/gencode.v19.pc_transcripts.fa --gtf /home/fogside/lncRNA/data/gencode/new_gencode.gtf -l 1 --fformat gtf -o ./coding_table_gencode -x Human_Hexamer.tsv
